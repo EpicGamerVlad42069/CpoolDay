73e0771012be28ca5a1084b77af01b7aca430107
EpicGamerVlad42069 <popvm2005@gmail.com>
2023-07-12 15:44:55 +0200

    gog

--- my_print_square.c
+++ my_print_square.c
@@ -1,12 +1,22 @@
 void print_char();
 
 void my_print_square(int size, char c){
-    int i;
-    int j;
-    for (i = 0; i < size; i++){
-        for (j = 0; j < size; j++){
-            print_char(c);
+    if (size > 0){
+        int i;
+        int j;
+        for (i = 0; i < size; i++){
+            for (j = 0; j < size; j++){
+                print_char(c);
+            }
+            print_char('\n');
         }
-        print_char('\n');
     }
+}
+
+
+
+int main(void)
+{
+my_print_square(-2, 'g');
+return 0;
 }
