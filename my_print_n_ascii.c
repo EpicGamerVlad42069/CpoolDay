c5604106a621dae4d975f9690f7b11a760391a3f
EpicGamerVlad42069 <popvm2005@gmail.com>
2023-07-12 12:03:34 +0200

    hiiii:3

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -1,6 +1,12 @@
 void print_char();
 void my_print_n_ascii(int howMany){
     howMany = howMany + 33;
+    if (howMany > 127){
+        return;
+    }
+    if (howMany < 33){
+        return;
+    }
     int ind;
     for (ind = 33; ind < howMany; ++ind){
         print_char(ind);
